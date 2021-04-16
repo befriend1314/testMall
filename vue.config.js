fdf49a1d375ee92402795ce365641becd45a68c4
liangjianfeng <769970587@qq.com>
2021-04-16 23:37:45 +0800

    fix 404

--- vue.config.js
+++ vue.config.js
@@ -16,11 +16,5 @@ module.exports = {
       args[0].title = '商城';
       return args;
     })
-  },
-  devServer: {
-    // history模式下的url会请求到服务器端，但是服务器端并没有这一个资源文件，就会返回404，所以需要配置这一项
-    historyApiFallback: {
-      index: '/index.html' //与output的publicPath
-    },
   }
 }
